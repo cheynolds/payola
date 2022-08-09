1a81991a2713375451a3abf1604c208fa35e1d81
Chris <github@cheynolds.com>
2022-08-08 18:23:25 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,3 @@
-// Add file IO to program 4. Create a plain-text file that will store employee information from the program code. 
-// The program will have the ability to load the array with the data from the file.
-
-// When the user chooses to load from a file, ask for the name of the file, then load the data into the employee array. 
-// If they choose keyboard, load the data as was done previously. Note that the application is going to want to use the 
-// folder where the .exe file is located when looking for the file, unless we type in a full or relative path. 
-// Using the debug folder is fine for our purposes.
-
-// Once the data is loaded into our array, either from an already existing file, or from keyboard input, 
-// we’ll then have our menu from the previous assignment. Before the program exits, you will need to ask for 
-// the name of the file to save the data to. If we loaded from a file already, 
-// we’ll use the same file and overwrite the contents.
-
-// We don’t need to cover deleting employee data. 
-// We can also assume that we only need to handle at most 10 employees worth of data.
-
-
 #include <stdio.h>
 #include <string.h> 
 #include <stdlib.h>
